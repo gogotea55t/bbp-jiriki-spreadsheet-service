9f493cad4144c7592227570e5d58034839df6540
gogotea55t <chirigami05@gmail.com>
2020-03-27 22:57:18 +0900

    feat: スプレッドシートに得点を書き込む機能を追加

--- src/main/java/io/github/gogotea55t/jirikisp/bbpjirikispreadsheetservice/Reciever.java
+++ src/main/java/io/github/gogotea55t/jirikisp/bbpjirikispreadsheetservice/Reciever.java
@@ -1,16 +1,24 @@
 package io.github.gogotea55t.jirikisp.bbpjirikispreadsheetservice;
 
-import java.util.LinkedHashMap;
-
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import io.github.gogotea55t.jiriki.domain.request.ScoreRequest;
+import io.github.gogotea55t.jirikisp.bbpjirikispreadsheetservice.service.SheetService;
 
 @RabbitListener(queues = "jiriki-bbp-spreadsheet")
 public class Reciever {
+  @Autowired
+  private SheetService sheetService;
+
   @RabbitHandler
   public void receive(ScoreRequest in) {
-	System.out.println("[x] Recieved: '" + in + "'");
+    System.out.println("[x] Recieved: '" + in + "'");
+    try {
+      sheetService.updateScore(in);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 }
